5e2892ed9e642518a211f0115421b21da35971e0
Andrey Shepilov <shepilovandrey@gmail.com>
2019-12-05 10:13:13 +0200

    Updates

---
+++ precache-manifest.59a25978f5479d647caceb073372489f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06f42654eb6ac6d4438272185d7d4a6",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "1f96ab32b037979b7943",
+    "url": "/weather/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d064b4cb9a5253ca1b82",
+    "url": "/weather/static/css/main.48dccdeb.chunk.css"
+  },
+  {
+    "revision": "1f96ab32b037979b7943",
+    "url": "/weather/static/js/2.b6a142ec.chunk.js"
+  },
+  {
+    "revision": "d064b4cb9a5253ca1b82",
+    "url": "/weather/static/js/main.70d61c9c.chunk.js"
+  },
+  {
+    "revision": "a1de7c2da6f8e23dcd88",
+    "url": "/weather/static/js/runtime-main.c7798bc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fd371943bbc03c8a663bca0930e357e.js"
+  "/weather/precache-manifest.59a25978f5479d647caceb073372489f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

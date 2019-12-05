d989d205cf905e4318128da2eccf36611e3da54b
Andrey Shepilov <shepilovandrey@gmail.com>
2019-12-05 10:10:40 +0200

    Updates

---
+++ precache-manifest.3fd371943bbc03c8a663bca0930e357e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e30f1c7191b22a618b0a271411752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f96ab32b037979b7943",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8a9988e05cd7f1fc132c",
+    "url": "/static/css/main.48dccdeb.chunk.css"
+  },
+  {
+    "revision": "1f96ab32b037979b7943",
+    "url": "/static/js/2.b6a142ec.chunk.js"
+  },
+  {
+    "revision": "8a9988e05cd7f1fc132c",
+    "url": "/static/js/main.1eb2eef0.chunk.js"
+  },
+  {
+    "revision": "99e4832d814e7d1d1861",
+    "url": "/static/js/runtime-main.caac9b04.js"
+  }
+]);
